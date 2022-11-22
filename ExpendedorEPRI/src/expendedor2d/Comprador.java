73dc07bc550ee7787e152f7aa06011178bdd19bf
Ma7iaz <maescobar2020@udec.cl>
2022-11-22 01:06:36 -0300

    cambios y adaptacion de comprador

--- ExpendedorEPRI/src/expendedor2d/Comprador.java
+++ ExpendedorEPRI/src/expendedor2d/Comprador.java
@@ -1,126 +1,192 @@
 package expendedor2d;
 
 import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
-public class Comprador {
-    private int vuelto, cualBeb, ordenDepos=0, ordenDeposMon=0, aux=-1;
-    private String tomar;
-    private int x,  y, altura, ancho;
-    private Color color;
-    private Bebida lata;
-    private Deposito bebidaDepos;
-    private DepositoVuelto vueltoDepos;
+
+
+
+import javax.swing.JPanel;
+
+public class Comprador extends JPanel {
+    private int vuelto;
+    private String sabor;                 //La rubrica decia ocupar solo 2 variables
+                  //Pero no puedo no poner la referencia a la maquina ---> se usa en sacarYcontar
     
-    public Comprador(int x, int y, Color color) {
-        this.x=x;this.y=y;
-        altura = 400; ancho = 150; 
-        this.color = color;
-        bebidaDepos = new Deposito(x-30, y+200,30,100);
-        vueltoDepos = new DepositoVuelto(x+ancho-75, y+200, 75, 35);
+    
+    public Comprador(Moneda mo,int numbeb,Expendedor Exp) {
+        vuelto = 0;
+        Exp.comprarBebida(mo,numbeb);
+         
+        
+                       //Aqui ira el delay
+        
+        
+        
+        
+                                                        //QUITE EL DEPOSITO DE BEBIDAS
+                                                         //QUITE EL DEPOSITO DE VUELTO
     }
     public void paint(Graphics g){
-        g.setColor(color);
-        g.fillRect(x, y, ancho, altura);
-        if(bebidaDepos != null) bebidaDepos.paint(g);
-        if(vueltoDepos != null) vueltoDepos.paint(g);
-        g.setColor(Color.LIGHT_GRAY);        
-        g.fillRect(375, 300, 50, 35);        
-        g.setColor(Color.ORANGE);
-        g.drawString(""+100, 750, 70);
-        g.drawRect(750, 50, 30, 30);
-        g.setColor(Color.BLUE);
-        g.drawRect(790, 50, 30, 30);
-        g.drawString(""+500, 790, 70);
-        g.setColor(Color.BLACK);
-        g.drawRect(830, 50, 30, 30);
-        g.drawString(""+1000, 830, 70);
-        g.setColor(Color.green);
-        g.drawString(""+1500, 870, 70);
-        g.drawRect(870, 50, 30, 30);
-        g.setColor(Color.MAGENTA);
-        g.drawString("VUELTO", 376, 320);
-    }
-    public boolean estaEn(int x, int y){
-        boolean r = false;
-        if(x>=this.x && x<=x+50 && y>=this.y && y<=this.y+50){
-            r=true;
-        }
-        return r;
-    }
-    public void cualBebida(int cual){        
-        cualBeb = cual;
-    }
-    public int eleccion(){
-        return cualBeb; 
-    }
-    public Moneda cualMoneda(Expendedor exp, int X, int Y){
-        Moneda aux;
-        this.aux++;
-        if(X>=750 && X<=780 && Y>=50 && Y<=780){
-            aux = new Moneda100(12345+this.aux,0,0);
-            return aux;
-        }else if(X>=790 && X<=820 && Y>=50 && Y<=780){
-            aux = new Moneda500(13346+this.aux,0,0);
-            return aux;
-        }else if(X>=830 && X<=860 && Y>=50 && Y<=780){
-            aux = new Moneda1000(14347+this.aux,0,0);
-            return aux;
-        }else if(X>=870 && X<=900 && Y>=50 && Y<=780){
-            aux = new Moneda1500(15348+this.aux,0,0);
-            return aux;
-        }else{
-            return null;
-        }       
-    }
-    public void hayBebida(Moneda m, int cualBebida, Expendedor exp){
-        lata = exp.comprarBebida(m, cualBebida);
-        if(lata != null){
-            tomar = lata.beber();
-        }else{                            
-            tomar = null;
-        }
-    }
-    public void calculaVuelto(Moneda m, Expendedor exp){
-        vuelto = 0;
-        for(int i = 0; true; i++){
-            Moneda monAux = exp.getVuelto();
-            if(monAux != null){
-                vuelto = vuelto + monAux.getValor();
-            }else{
-                break;
-            }            
-        }
-        if(lata == null){
-            if(m != null){
-                vuelto = m.getValor();
-            }
-        }
-    }
-    public void guardarBebida(Bebida lata){
-        if(lata != null){
-            lata.setXY(bebidaDepos.getX()+2, bebidaDepos.getY()+ordenDepos*lata.getLargo());
-            bebidaDepos.addBebida(lata);
-            ordenDepos++;
-        }                                     
-    }
-    public void guardarVuelto(Moneda mon){
-        if(mon != null){
-            mon.setXY(vueltoDepos.getX()+ordenDeposMon*3, vueltoDepos.getY()+2);  
-            vueltoDepos.addMoneda(mon, mon.getValor());
-            ordenDeposMon++;
-        }
-    }
-    public void tomarVuelto(Expendedor exp){
-        if(exp.getVuelto() != null){           
-            vueltoDepos.addMoneda(exp.getVuelto(), 1);
-        }
+        super.paint(g);
+        g.setColor(Color.WHITE);
+        g.drawString("Vuelto:$"+vuelto,850,120); 
+        
+        
+        
+
+
+                                           //OJO aqui agregar la imagen
     }
     
-    public int cuantoVuelto(){
-        return vuelto;
+    
+    public void animafinal(Graphics g){
+        
+        for(int i= 0; i<4 ; i++){
+            
+            anima(g,i);
+            
+        }
+        
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    public void anima(Graphics g, int a){
+    long time = 70; int x = 700; int y = 150; int i = 6;
+    int xb = 400; int yb = 144; x -= a*13*i;
+        ImageIcon d = new ImageIcon("src/img/013.png");   //drawea botones
+        g.drawImage(d.getImage(),x,y,243,330,this);
+        Delay(time);  
+        ImageIcon borrar = new ImageIcon("src/img/fanima.png"); 
+         g.drawImage(borrar.getImage(),xb,yb,584,340,this);    x -= i;
+        
+//OOO que soy weon pero si llama a la misma imagen
+    ImageIcon d1 = new ImageIcon("src/img/012.png");   //drawea botones
+        g.drawImage(d1.getImage(),x,y,243,330,this);      
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+    ImageIcon d2 = new ImageIcon("src/img/011.png");   //drawea botones
+        g.drawImage(d2.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+    ImageIcon d3 = new ImageIcon("src/img/010.png");   //drawea botones
+        g.drawImage(d3.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+    ImageIcon d4 = new ImageIcon("src/img/009.png");   //drawea botones
+        g.drawImage(d4.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+    ImageIcon d5 = new ImageIcon("src/img/008.png");   //drawea botones
+        g.drawImage(d5.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+   ImageIcon d6 = new ImageIcon("src/img/007.png");   //drawea botones
+        g.drawImage(d6.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        ImageIcon d7 = new ImageIcon("src/img/006.png");   //drawea botones
+        g.drawImage(d7.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        ImageIcon d8 = new ImageIcon("src/img/005.png");   //drawea botones
+        g.drawImage(d8.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        ImageIcon d9 = new ImageIcon("src/img/004.png");   //drawea botones
+        g.drawImage(d9.getImage(),x,y,243,330,this);
+        Delay(time); 
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        ImageIcon d10 = new ImageIcon("src/img/003.png");   //drawea botones
+        g.drawImage(d10.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        ImageIcon d11 = new ImageIcon("src/img/002.png");   //drawea botones
+        g.drawImage(d11.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        ImageIcon d12 = new ImageIcon("src/img/001.png");   //drawea botones
+        g.drawImage(d12.getImage(),x,y,243,330,this);
+        Delay(time);
+        g.drawImage(borrar.getImage(),xb,yb,584,340,this);   x -= i;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+   
+    
+    public void sacarYcontar(Expendedor Exp){    //SACAR TODOS LOS MONEAS Y UNIFICARLO   
+       Moneda mia = Exp.Cajamonedas.sacarMoneda();
+       
+       //while(mia != null){ 
+        if(mia != null)vuelto += 100;
+        //mia = Exp.Cajamonedas.sacarMoneda();    
+    //}
+    
+       //vuelto = total;
+       
+    }
+       
+       
+       
+                                             //QUITE EL GUARDA BEBIDA
+    
+                                                 //QUITE GUARDA VUELTO    
+    
+    
+    
+   
     
     public String queBebiste(){
-        return tomar;
+        return sabor;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void Delay(long millis){
+        long ini = System.currentTimeMillis();
+        while(System.currentTimeMillis() - ini < millis){
+            
+        }
+        
+        
+    }
+    
+    
+    
 }
