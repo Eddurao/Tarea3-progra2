58e00f2b347b09a162d6adc143656f2f8410ebd8
Ma7iaz <maescobar2020@udec.cl>
2022-11-22 00:56:57 -0300

    perfeccionamiento deposito

--- ExpendedorEPRI/src/expendedor2d/Deposito.java
+++ ExpendedorEPRI/src/expendedor2d/Deposito.java
@@ -3,52 +3,57 @@ package expendedor2d;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import javax.swing.JPanel;
 
-public class Deposito {
-    private int x,  y, largo, ancho;
-    private ArrayList <Bebida> bebida;
+public class Deposito {              //QUITARE ANCHO Y LARGO
+    private int code;
+    private int cargo;
+    private ArrayList <Bebida> ALbebidas;
     
-    public Deposito(int x, int y, int ancho, int largo){
-        this.x=x;this.y=y;
-        this.ancho = ancho; this.largo = largo;
-        bebida = new ArrayList <Bebida>();
+    public Deposito(int code, int cargo) {
+        this.code = code;
+        this.cargo = cargo;
+        ALbebidas = new ArrayList <>();
+        rellenar();
     }
+    
+    
+    
     public void paint(Graphics g){
-        g.setColor(Color.BLACK);
-        g.fillRect(x, y, ancho, largo);
-        if(!bebida.isEmpty() ){
-            for(int i=0; i<bebida.size(); i++){
-                bebida.get(i).paint(g);                
-            }
+        
+        for(int i=0;i<ALbebidas.size();i++){
+            (ALbebidas.get(i)).paint(g);
+            
+            
         }
+
+                                                  //AQUI HACER EL PRINTA BEBIDAS
+        
     }
-    public void addBebida(Bebida b){
-        if(b != null){
-            bebida.add(b);
-        }
+    public void rellenar(){//rellena con bebidas del tipo code, cantidad cargo
+       for(int i=0;i<cargo;i++){
+           if(ALbebidas.size() == cargo){break;} //si llega a tope cancela
+           if(code == 1){ALbebidas.add(new CocaCola(100,55+(155/cargo)*i));}  //AQUI SETEAR SUS POSICIONES
+           if(code == 2){ALbebidas.add(new Sprite(150,55+(155/cargo)*i));}
+           if(code == 3){ALbebidas.add(new Fanta(200,55+(155/cargo)*i));}
+           if(ALbebidas.size() == cargo){break;}  //si llega a tope cancela
+       } 
+                                //CAMBIAR : tira una sola bebida ????
+                                                 //ESTE ES EL ALMACENABEBIDAS
     }
     public Bebida getBebida(){
-        if(bebida.size() == 0)
+        if(ALbebidas.size() == 0)
             return null;
+                                                 //SACA BEBIDA
         else
-            return bebida.remove(0);
+            return ALbebidas.remove(0);
     }
-    public void setXY(int x, int y){
-        this.x=x;this.y=y;        
-    }
+    
+    
     public int getSize(){
-        return bebida.size();
+        return ALbebidas.size();
     }
-    public int getX(){
-        return this.x;
-    }
-    public int getY(){
-        return this.y;
-    }
-    public int getLargo(){
-        return this.largo;
-    }
-    public int getAncho(){
-        return this.ancho;
-    }
+    
+    
+    
 }
