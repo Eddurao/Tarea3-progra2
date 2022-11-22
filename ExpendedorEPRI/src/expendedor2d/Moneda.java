1902b466eea303e2c7d6a0297e77b2f6f0722891
Ma7iaz <maescobar2020@udec.cl>
2022-11-22 00:46:25 -0300

    mejoramiento de moneda

--- ExpendedorEPRI/src/expendedor2d/Moneda.java
+++ ExpendedorEPRI/src/expendedor2d/Moneda.java
@@ -1,15 +1,24 @@
 package expendedor2d;
 
 import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-abstract class Moneda {
-    private int numSerie,x,y;
-    public Moneda(int numSerie, int x,int y) {
-    }
-    public int getSerie(){
+abstract class Moneda extends JPanel {        //Me parece que no tiene que ser tan abstracto ??  aaaa nooooo si puede
+    public int numSerie,x,y;       // QUE ES EL NUMSERIE !!!! NO LE DA VALOR A NUMSERIE
+    public Moneda( int x,int y) {
+    this.x = x; this.y = y;
+    
+    }                                //ES ABSTARCTO.. PERO AQUI DEFINIR EL PUNTERO VALOR A DECIMAL
+    public int getSerie(){          //TQV con el CODIGO HASH ????
         return numSerie;
+    }                           /////JAJAJAJ ESTUVE PEGADO AQUI Y ERA PORQUE NO PUSE EL CONSTRUCTOR 
+    public void setSerie(){
+                                     //Esto que hice yo te crea la serie/Ponelo arriba en el constructor
+       
+    
     }
-    public void setXY(int x, int y){
+    public void setXY(int x, int y){     //QUITE NUMSERIE DE PARAMETRO PORQUE LO PUEDE CONSTRUIR SOLITO
         this.x = x; this.y = y;
     }
     public int getX(){
@@ -19,89 +28,91 @@ abstract class Moneda {
         return this.y;
     }
     public abstract int getValor();
+    
+   
 
-    public abstract void paint(Graphics g, int x, int y);
+    //public abstract void paint(Graphics g);
+    
+    public abstract void paint(Graphics g); 
+                            //QUITE EL X E Y PQ YA LO TENENMOS
+                           //AQUi poner imagen de cada moneda
+        
+    
+    
         
 }
 
 class Moneda100 extends Moneda{
-    private int numSerie, x,y;
-    public Moneda100(int serie,int x, int y){
-        super(serie,x,y);
-        numSerie = serie;
+                      //AQUI REPITE LAS PROPIEDADES DE LA MONEDA !!!!
+    public Moneda100(int x, int y){
+        super(x,y);
+        //super.numSerie = .....   COMPLETAER ESTA OBTENER EL HASH CODE ???
     }
     @Override
     public int getValor(){
         return 100;
     }
+        //NO NECESITO COLOCAR PAINT PQ LO HEREDA DE ARRIBA
     @Override
-    public void paint(Graphics g, int x, int y){
-        this.x=x; this.y=y;
-        g.setColor(Color.ORANGE);
-        g.fillOval(this.x,this.y,30,30);
-        g.setColor(Color.WHITE);
-        g.drawString(""+numSerie, this.x+30/7, this.y+15);
+    
+    public void paint(Graphics g){
+        
+        
+         ImageIcon mon100 = new ImageIcon("src/img/100pesos.png");  
+        g.drawImage(mon100.getImage(),super.x ,super.y ,40,40,this); 
+        
+        
     }
+    
+    
 }
 
 class Moneda500 extends Moneda{
     
-   private int numSerie, x,y;
-    public Moneda500(int serie,int x, int y){
-        super(serie,x,y);
-        numSerie = serie;
+   
+    public Moneda500(int x, int y){
+        super(x,y);
+        super.setSerie();
     }
     @Override
     public int getValor(){
         return 500;
     }
     @Override
-    public void paint(Graphics g, int x, int y){
-        this.x=x; this.y=y;
-        g.setColor(Color.BLUE);
-        g.fillOval(this.x,this.y,30,30);
-        g.setColor(Color.WHITE);
-        g.drawString(""+numSerie, this.x+30/7, this.y+15);
+    public void paint(Graphics g){            //QUITE TODOS LOS X E Y ERAN REDUNDANTES
+        
     }
 }
 
 class Moneda1000 extends Moneda{
     
-   private int numSerie, x,y;
-    public Moneda1000(int serie,int x, int y){
-        super(serie,x,y);
-        numSerie = serie;
+   
+    public Moneda1000(int x, int y){
+        super(x,y);
+        super.setSerie();
     }
     @Override
     public int getValor(){
         return 1000;
     }
     @Override
-    public void paint(Graphics g, int x, int y){
-        this.x=x; this.y=y;
-        g.setColor(Color.BLACK);
-        g.fillOval(this.x,this.y,30,30);
-        g.setColor(Color.WHITE);
-        g.drawString(""+numSerie, this.x+30/7, this.y+15);
+    public void paint(Graphics g){
+        
     }
 }
 class Moneda1500 extends Moneda{
     
-  private int numSerie, x,y;
-    public Moneda1500(int serie,int x, int y){
-        super(serie,x,y);
-        numSerie = serie;
+  
+    public Moneda1500(int x, int y){
+        super(x,y);
+        super.setSerie();
     }
     @Override
     public int getValor(){
         return 1500;
-    }
+    }                                        //UNIFICAR EL GRAPHICS G
     @Override
-    public void paint(Graphics g, int x, int y){
-        this.x=x; this.y=y;
-        g.setColor(Color.green);
-        g.fillOval(this.x,this.y,30,30);
-        g.setColor(Color.WHITE);
-        g.drawString(""+numSerie, this.x+30/7, this.y+15);
+    public void paint(Graphics g){
+        
     }
 }
