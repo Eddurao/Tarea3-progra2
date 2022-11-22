efd8fd3c08de5ff7bdf9bd524041a804ee877697
Mathias Escobar Fernandez <matiasescobarfernandez@gmail.com>
2022-11-21 23:45:34 -0300

    mejoramiento de Bebida por rubrica

--- ExpendedorEPRI/src/expendedor2d/Bebida.java
+++ ExpendedorEPRI/src/expendedor2d/Bebida.java
@@ -1,28 +1,30 @@
 package expendedor2d;
 
-import java.awt.*;
 
-abstract public class Bebida {
-    private int numSerie;
-    private int x, y, ancho, largo;
-      //Hice cambios
+
+import java.awt.*;     //Falta La imagen(paint) y el sonido
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+abstract public class Bebida extends JPanel {
+    public int numSerie;
+    public int x, y;
     
-    public Bebida(int numSerie, int x, int y, Color color){
+    
+    public Bebida(int x, int y){
         this.x = x; this.y = y;
-        this.numSerie = numSerie;
-    
-        this.ancho = 26; this.largo = 10;
+        this.numSerie = 1;    
     }
     public int getSerie(){
         return this.numSerie;
     }
     public abstract String beber();
 
-    public void paint(Graphics g) {
-     
-        g.drawRect(x, y, ancho, largo);
-        g.drawString(""+numSerie, x+ancho/7, y+largo);
-    }
+    public abstract void paint(Graphics g);   //OJO aqui pondremos la imagen
+        
+        
+        
+    
     public void setXY(int x, int y){        
         this.x = x; this.y = y;
     }
@@ -32,42 +34,59 @@ abstract public class Bebida {
     public int getY(){
         return this.y;
     }
-    public int getLargo(){
-        return this.largo;
-    }
-    public int getAncho(){
-        return this.ancho;
-    }
+    
+    
 }
 
 class Sprite extends Bebida{
     
-    public Sprite(int numSerie, int x, int y, Color color){
-        super(numSerie, x, y, color);
+    public Sprite(int x, int y){
+        super(x, y);
     }
     @Override
     public String beber(){
-        return "Sprite";
+        return "Sprite";            //Customizar
+    }
+    @Override
+    public void paint(Graphics g){
+        
+        ImageIcon i2 = new ImageIcon("src/img/spritef.png");   //drawea botones
+        g.drawImage(i2.getImage(),super.x + (-7*super.y/55+26),super.y,50 + 7*super.y/55 - 26,100 + 14*super.y/55 - 26*2,this);      //g.drawImage(i2.
+        //Esto es para que las bebidas de mas arriba sean mas chicas emulando el espacio
     }
 }
 
 class CocaCola extends Bebida{
     
-    public CocaCola(int numSerie, int x, int y, Color color){
-        super(numSerie, x, y, color);
+    public CocaCola(int x, int y){
+        super(x, y);
     }
     @Override
     public String beber(){
         return "CocaCola";
     }
+    @Override
+    public void paint(Graphics g){
+        
+        ImageIcon i2 = new ImageIcon("src/img/colaf.png");   //drawea botones
+        g.drawImage(i2.getImage(),super.x + (-7*super.y/55+26),super.y,50 + 7*super.y/55 - 26,100 + 14*super.y/55 - 26*2,this);      //g.drawImage(i2.
+        
+    }
 }
 class Fanta extends Bebida{
     
-    public Fanta(int numSerie, int x, int y, Color color){
-        super(numSerie, x, y, color);
+    public Fanta(int x, int y){
+        super(x, y);
     }
     @Override
     public String beber(){
         return "Fanta";
+    }
+    @Override
+    public void paint(Graphics g){
+        
+        ImageIcon i2 = new ImageIcon("src/img/fantaf.png");   //drawea botones
+        g.drawImage(i2.getImage(),super.x + (-7*super.y/55+26),super.y,50 + 7*super.y/55 - 26,100 + 14*super.y/55 - 26*2,this);      //g.drawImage(i2.
+        
     }
 }
